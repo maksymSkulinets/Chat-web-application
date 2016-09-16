e969d2f4ad3eab96a059f032c608b7d86ff5260b
maksym.skulinets <maksym.skulinets@gmail.com>
2016-09-16 23:12:20 +0300

    Implementation of handlers was removed.

--- webapp/src/main/java/com/teamdev/javaclasses/HandlerContainer.java
+++ webapp/src/main/java/com/teamdev/javaclasses/HandlerContainer.java
@@ -1,6 +1,6 @@
 package com.teamdev.javaclasses;
 
-import com.teamdev.javaclasses.handler.*;
+import com.teamdev.javaclasses.handler.Handler;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,19 +10,11 @@ class HandlerContainer {
     private final Map<RequestContext, Handler> registry =
 
             new HashMap<RequestContext, Handler>() {{
-                put(new RequestContext("/api/registration", "POST"), new SignUpHandler());
-                put(new RequestContext("/api/login", "POST"), new LoginHandler());
-                put(new RequestContext("/api/create_chat", "POST"), new ChatCreationHandler());
-                put(new RequestContext("/api/add_member", "POST"), new AddMemberToChatHandler());
-                put(new RequestContext("/api/remove_member", "POST"), new RemoveMemberHandler());
-                put(new RequestContext("/api/send_message", "POST"), new SendMessageHandler());
+
             }};
 
 
-    public HandlerContainer() {
-    }
-
-    public Handler getHandler(RequestContext requestContext) {
+    Handler getHandler(RequestContext requestContext) {
         return registry.get(requestContext);
     }
 }
