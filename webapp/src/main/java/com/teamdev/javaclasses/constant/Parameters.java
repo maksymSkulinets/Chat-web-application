db614fd14648389ab54197ceb15b92d9b776cd7f
maksym.skulinets <maksym.skulinets@gmail.com>
2016-09-20 18:03:53 +0300

    Constants was moved to separate classes.

---
+++ webapp/src/main/java/com/teamdev/javaclasses/constant/Parameters.java
@@ -0,0 +1,9 @@
+package com.teamdev.javaclasses.constant;
+
+public class Parameters {
+    public final static String NICKNAME = "nickname";
+    public final static String PASSWORD = "password";
+    public final static String VERIFY_PASSWORD = "verifyPassword";
+    public final static String USER_ID = "userId";
+    public final static String WARNING_MESSAGE = "warningMessage";
+}
---
+++ webapp/src/main/java/com/teamdev/javaclasses/constant/Uri.java
@@ -0,0 +1,5 @@
+package com.teamdev.javaclasses.constant;
+
+public class Uri {
+    public final static String REGISTRATION_URI = "/chat/registration";
+}
